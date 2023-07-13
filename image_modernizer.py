63a758161269cbb8f80c22d70a6441d2bca9a01f
orencohendev <oren@orencreates.com>
2023-07-13 20:21:23 +0300

    changing to image modernizer

--- image_modernizer.py
+++ image_modernizer.py
@@ -10,25 +10,33 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.master.title("Welcome to PNG Converter")
+        self.master.title("Welcome to Image Modernizer")
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        self.title = tk.Label(self, text="Welcome to PNG Converter", font=("Arial", 20))
-        self.title.pack(side="top")
+        self.title = tk.Label(self, text="Welcome to Image Modernizer", font=("Arial", 20))
+        self.title.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
-        self.subtitle = tk.Label(self, text="Please click the Browse button to select your file", font=("Arial", 14))
-        self.subtitle.pack(side="top")
+        self.subtitle = tk.Label(self, text="Please click the Browse button to select the\n file you want to modernize or click exit.", font=("Arial", 14))
+        self.subtitle.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
         
         self.empty_line = tk.Label(self, text="")
-        self.empty_line.pack(side="top")
+        self.empty_line.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
         self.browse = tk.Button(self, text="Browse", font=("Arial", 20), height=2, width=10)
         self.browse["command"] = self.open_file_dialog
-        self.browse.pack(side="top")
+        self.browse.grid(row=3, column=0, padx=10, pady=10)
+
+        self.exit = tk.Button(self, text="Exit", font=("Arial", 20), height=2, width=10)
+        self.exit["command"] = self.exit_gracefully
+        self.exit.grid(row=3, column=1, padx=10, pady=10)
 
         self.progress = ttk.Progressbar(self, length=200, mode='indeterminate')
+
+    def exit_gracefully(self):
+        self.master.destroy()
+
 
     def open_file_dialog(self):
         filepath = filedialog.askopenfilename(filetypes=[("PNG Files", "*.png"), ("JPEG Files", "*.jpg"), ("JPEG Files", "*.jpeg")])
